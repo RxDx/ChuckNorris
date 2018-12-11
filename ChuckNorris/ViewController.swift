c23161f34853988ef0dce1252da68acb803772a2
Rodrigo Dumont <rodrigo@rxdx.com.br>
2018-12-11 21:23:53 -0200

    Initial Commit

---
+++ ChuckNorris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorris
+//
+//  Created by Rodrigo Dumont on 11/12/18.
+//  Copyright © 2018 Rodrigo Dumont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
